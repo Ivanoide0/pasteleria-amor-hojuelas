e8082c33596ca7f12bcda96cb29f05fa0bdd78ff
Ricardo <202300126@upqroo.edu.mx>
2026-03-29 14:04:36 -0500

    seccion ocasiones

--- js/main.js
+++ js/main.js
@@ -15,3 +15,17 @@ dropdowns.forEach(drop => {
     drop.classList.toggle('active');
   });
 });
+
+const reveals = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("active");
+    }
+  });
+}, {
+  threshold: 0.2
+});
+
+reveals.forEach(el => observer.observe(el));
